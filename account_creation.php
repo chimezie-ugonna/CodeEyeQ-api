8e6c511888e8069b608c26c23eb5add73598c1f1
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-02 14:23:40 +0100

    Still testing postgres database connection

--- account_creation.php
+++ account_creation.php
@@ -11,7 +11,7 @@ try{
 
 
     //create a pdo instance
-    $pdo = new PDO($dsn, "uaqteoyzlwegvo", "b210289ab147c6c6c2ff023fc8b91a61cd29708140f2b06e9c9dbaf06da94e42");
+    $pdo = new PDO($dsn, "uaqteoyzlwegvog", "b210289ab147c6c6c2ff023fc8b91a61cd29708140f2b06e9c9dbaf06da94e42");
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
