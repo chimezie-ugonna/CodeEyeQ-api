aceffbb36ef17d37a2c48beb35b3b6007c7b6407
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-02 14:25:39 +0100

    removed comments.

--- account_creation.php
+++ account_creation.php
@@ -1,7 +1,6 @@
 <?php
 
 try{
-    //Set DSN data source name
     $dsn = "pgsql:host=ec2-44-194-54-186.compute-1.amazonaws.com;
     port=5432;
     dbname=dco8bgoaljvb7;
@@ -10,7 +9,6 @@ try{
     sslmode=require;";
 
 
-    //create a pdo instance
     $pdo = new PDO($dsn, "uaqteoyzlwegvog", "b210289ab147c6c6c2ff023fc8b91a61cd29708140f2b06e9c9dbaf06da94e42");
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
