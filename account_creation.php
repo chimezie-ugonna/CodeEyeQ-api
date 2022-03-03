d1c2ef1b76048566a49e692870aafe9cb88e1e8f
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-03 15:11:29 +0100

    test

--- account_creation.php
+++ account_creation.php
@@ -2,16 +2,7 @@
 require "database_connection.php";
 $status = array();
 if($connection){
-    ini_set('display_errors', 1);
-    error_reporting(E_ALL);
-    try {
-        $user_id = generate_user_id($pdo);
-        echo "User Id" . $user_id;
-
-    } catch(Exception $e) {
-        echo $e->getMessage();
-    }
-    /*$ciphering = "AES-128-CTR";
+    $ciphering = "AES-128-CTR";
     $encryption_iv = (string)rand(1000000000000000,9999999999999999);
     $encryption_key = (string)rand(1000000000000000,9999999999999999);
     $encryption_iv_ = base64_encode($encryption_iv);
@@ -40,7 +31,7 @@ if($connection){
     }
     $email = openssl_encrypt("ugiezie@gmail.com", $ciphering, $encryption_key, 0, $encryption_iv);
 
-    $user_id = generate_user_id();
+    $user_id = generate_user_id($pdo);
 
     $password = openssl_encrypt("1234abcd", $ciphering, $encryption_key, 0, $encryption_iv);
     $time_zone = "Africa/Lagos";
@@ -50,7 +41,7 @@ if($connection){
 
     $query = "insert into users (user_id, email, first_name, last_name, password, image_status, image_path, gender, dob, encryption_key, encryption_iv, date_created, time_created, time_zone) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $statement = $pdo->prepare($query);
-    $statement->execute(array($user_id, $email, $first_name, $last_name, $password, "default", "", "", "", $encryption_key_, $encryption_iv_, $date, $time, $time_zone));*/
+    $statement->execute(array($user_id, $email, $first_name, $last_name, $password, "default", "", "", "", $encryption_key_, $encryption_iv_, $date, $time, $time_zone));
 
     $status["response"] = "Done";
 }else{
@@ -60,7 +51,6 @@ echo json_encode($status);
 $pdo = null;
 
 function generate_user_id($pdo){
-
     $user_id = "";
     $alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $num = array();
@@ -71,13 +61,11 @@ function generate_user_id($pdo){
         $user_id = $user_id . $num[$i];
     }
 
-    $user_id = "M1Y2M6V7G6I0K4Q2V6G4E7T5";
     $query = "select * from users where user_id = ?";
     $statement = $pdo->prepare($query);
     $statement->execute(array($user_id));
     $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
     if($result > 0){
-        echo "user id used before </br>";
         generate_user_id($pdo);
     }else{
         return $user_id;
