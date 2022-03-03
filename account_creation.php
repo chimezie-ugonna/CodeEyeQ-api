be258f8b40664e72dfee55be7807b73918227bf5
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-03 06:01:36 +0100

    Testing database connection.

--- account_creation.php
+++ account_creation.php
@@ -1,21 +1,44 @@
 <?php
+require "database_connection.php";
+date_default_timezone_set('Africa/Lagos');
+$status = array();
+if($connection){
+    $ciphering = "AES-128-CTR";
+    $encryption_iv = (string)rand(1000000000000000,9999999999999999);
+    $encryption_key = (string)rand(1000000000000000,9999999999999999);
+    $encryption_iv_ = base64_encode($encryption_iv);
+    $encryption_key_ = base64_encode($encryption_key);
 
-try{
-    $dsn = "pgsql:host=ec2-44-194-54-186.compute-1.amazonaws.com;
-    port=5432;
-    dbname=dco8bgoaljvb7;
-    user=uaqteoyzlwegvo;
-    password=b210289ab147c6c6c2ff023fc8b91a61cd29708140f2b06e9c9dbaf06da94e42;
-    sslmode=require;";
+    $account_name_encryption = openssl_encrypt($account_name, $ciphering, $ed_key, 0, $ed_iv);
+    $full_name = addslashes($_POST["full_name"]);
+    $full_name_split = explode(" ", $full_name);
+    if(count($full_name_split) > 1){
+        $first_name = openssl_encrypt($full_name_split[0], $ciphering, $encryption_key, 0, $encryption_iv);
+        $last_name = openssl_encrypt($full_name_split[1], $ciphering, $encryption_key, 0, $encryption_iv);
+    }else{
+        $first_name = "";
+        $last_name = "";
+    }
+    $email = openssl_encrypt(addslashes($_POST["email"]), $ciphering, $encryption_key, 0, $encryption_iv);
 
+    $alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $code = array();
+    for ($i = 0; $i < 3; $i++) {
+        $code[$i] = $alphabets[rand(0,25)] . (string)rand(0,9);
+    }
+    $user_id = $code[0] . $code[1] . $code[2];
 
-    $pdo = new PDO($dsn, "uaqteoyzlwegvo", "b210289ab147c6c6c2ff023fc8b91a61cd29708140f2b06e9c9dbaf06da94e42");
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Connection success';
+    $password = openssl_encrypt(addslashes($_POST["password"]), $ciphering, $encryption_key, 0, $encryption_iv);
+    $date = openssl_encrypt(date("F j, Y"), $ciphering, $encryption_key, 0, $encryption_iv);
+    $time = openssl_encrypt(date("g:i A"), $ciphering, $encryption_key, 0, $encryption_iv);
+
+    $sql = "insert into users (user_id, email, first_name, last_name, password, image_status, image_path, gender, dob, date_created, time_created) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(array($user_id, $email, $first_name, $last_name, $password, "default", "", "", "", $date, $time));
+
+    $result = $statement->fetchAll();
+}else{
+    $status["response"] = "connection failed";
 }
-catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
-}
+echo json_encode($status);
 ?>
