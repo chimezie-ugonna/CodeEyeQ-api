4afcba45704bd5ca72dbd99d570203926b9f3344
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-10 21:18:17 +0100

    test

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ try {
     }*/
 
     $ciphering = "AES-128-CTR";
-    $encryption_iv = base64_decode("ODc5MDI2ODc5NTM3NzE5MA==");
-    $encryption_key = base64_decode("MTU4NTExODM5MzM1NTI1OQ==");
-    echo "device_brand: " . openssl_decrypt("u5twsxNEww==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "device_model: " . openssl_decrypt("m7cwgVcYkdU=", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "app_version: " . openssl_decrypt("np9vsw9FyrOnAxXK3Q==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "time_zone: " . openssl_decrypt("iZxvqQVLi9/3SkqX", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "date: " . openssl_decrypt("hZtvow4Knb+2HxXW3w==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "time: " . openssl_decrypt("+sAv+EZr6Q==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
+    $encryption_iv = base64_decode("NzQwNDAxMjY2ODgzMzczNw==");
+    $encryption_key = base64_decode("MTczNTg2ODg4MDMyNDM0Nw==");
+    echo "device_brand: " . openssl_decrypt("AC9y7xr0lg==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
+    echo "device_model: " . openssl_decrypt("IAMy3V6oxCw=", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
+    echo "app_version: " . openssl_decrypt("JStt7wb1n0qSb00s7A==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
+    echo "time_zone: " . openssl_decrypt("Miht9Qz73ibCJhJx", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
+    echo "date: " . openssl_decrypt("Pi9t/we6wFqPYU8y7l4=", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
+    echo "time: " . openssl_decrypt("Qnwlr1e6sCc=", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
 
 } catch(Exception $e) {
     echo $e->getMessage();
