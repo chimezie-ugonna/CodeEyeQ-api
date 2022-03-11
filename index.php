c0803af502555371405c594ae929ebba0d9e9563
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-11 17:59:09 +0100

    database fixes.

--- index.php
+++ index.php
@@ -3,24 +3,14 @@ require "database_connection.php";
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 try {
-    /*$query = "select * from login_info where date = ?";
+    /*$query = "select * from login_info where user_id = ?";
     $statement = $pdo->prepare($query);
-    $statement->execute(array("hZtvow4Knb+2HxXW3w=="));
+    $statement->execute(array("fbD3WGeem7cmZBUmSU94LQy7Cn52"));
     $result = $statement->fetch_assoc();
     while ($row = $result) {
         echo $row['time'];
         break;
     }*/
-
-    $ciphering = "AES-128-CTR";
-    $encryption_iv = base64_decode("NDM1NTI0Mjc2MjgyMTk3OQ==");
-    $encryption_key = base64_decode("NzY1MTcxNzYzNzM2OTY0Ng==");
-    echo "device_brand: " . openssl_decrypt("UjZzvyYjHA==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "device_model: " . openssl_decrypt("chozjWJ/TuI=", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "app_version: " . openssl_decrypt("dzJsvzoiFYQ7zG7U8A==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "time_zone: " . openssl_decrypt("YDFspTAsVOhrhTGJ", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "date: " . openssl_decrypt("bDZsrzttSpQmwmzK8oc=", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
-    echo "time: " . openssl_decrypt("EG0s9XMMNg==", $ciphering, $encryption_key, 0, $encryption_iv) . "<br/>";
 
 } catch(Exception $e) {
     echo $e->getMessage();
--- join.php
+++ join.php
@@ -21,7 +21,7 @@ if($connection){
     $time = openssl_encrypt(date("g:i A"), $ciphering, $encryption_key, 0, $encryption_iv);
 
     $full_name_split = explode(" ", $full_name);
-    if(count($full_name_split) > 0){
+    if(count($full_name_split) > 0 && $full_name_split != ""){
         $first_name = openssl_encrypt($full_name_split[0], $ciphering, $encryption_key, 0, $encryption_iv);
         $ln = "";
         if(count($full_name_split) > 1){
@@ -39,15 +39,21 @@ if($connection){
             $last_name = "";
         }
     }
-    $email = openssl_encrypt($email, $ciphering, $encryption_key, 0, $encryption_iv);
+    if($email != ""){
+        $email = openssl_encrypt($email, $ciphering, $encryption_key, 0, $encryption_iv);
+    }
     $device_brand = openssl_encrypt($device_brand, $ciphering, $encryption_key, 0, $encryption_iv);
     $device_model = openssl_encrypt($device_model, $ciphering, $encryption_key, 0, $encryption_iv);
     $app_version = openssl_encrypt($app_version, $ciphering, $encryption_key, 0, $encryption_iv);
     $time_zone = openssl_encrypt($time_zone, $ciphering, $encryption_key, 0, $encryption_iv);
 
-    $query = "insert into users (user_id, email, first_name, last_name, image_status, image_path, gender, dob, encryption_key, encryption_iv, date_created, time_created, time_zone, theme) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $query = "insert ignore into users (user_id, email, first_name, last_name, image_status, image_path, gender, dob, encryption_key, encryption_iv, date_created, time_created, time_zone, theme) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $statement = $pdo->prepare($query);
     $statement->execute(array($user_id, $email, $first_name, $last_name, "default", "", "", "", $encryption_key_, $encryption_iv_, $date, $time, $time_zone, "system"));
+
+    $query = "delete from login_info where user_id = ?";
+    $statement = $pdo->prepare($query);
+    $statement->execute(array($user_id));
 
     $query = "insert into login_info (user_id, device_token, device_brand, device_model, app_version, date, time, time_zone, encryption_key, encryption_iv) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     $statement = $pdo->prepare($query);
