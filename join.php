94549743e5ab9a29937fba47f277fe3489fe5d8e
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-18 09:46:03 +0100

    added timestamp to tables

--- join.php
+++ join.php
@@ -16,10 +16,6 @@ if($connection){
         $device_brand = addslashes($_POST["device_brand"]);
         $device_model = addslashes($_POST["device_model"]);
         $app_version = addslashes($_POST["app_version"]);
-        $time_zone = addslashes($_POST["time_zone"]);
-        date_default_timezone_set($time_zone);
-        $date = openssl_encrypt(date("F j, Y"), $ciphering, $encryption_key, 0, $encryption_iv);
-        $time = openssl_encrypt(date("g:i A"), $ciphering, $encryption_key, 0, $encryption_iv);
 
         if($full_name != ""){
             $full_name_split = explode(" ", $full_name);
@@ -49,19 +45,18 @@ if($connection){
         $device_brand = openssl_encrypt($device_brand, $ciphering, $encryption_key, 0, $encryption_iv);
         $device_model = openssl_encrypt($device_model, $ciphering, $encryption_key, 0, $encryption_iv);
         $app_version = openssl_encrypt($app_version, $ciphering, $encryption_key, 0, $encryption_iv);
-        $time_zone = openssl_encrypt($time_zone, $ciphering, $encryption_key, 0, $encryption_iv);
 
-        $query = "insert into users (user_id, email, first_name, last_name, image_status, image_path, gender, dob, encryption_key, encryption_iv, date_created, time_created, time_zone, theme) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) on conflict (user_id) do nothing";
+        $query = "insert into users (user_id, email, first_name, last_name, image_status, image_path, gender, dob, encryption_key, encryption_iv, theme) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) on conflict (user_id) do nothing";
         $statement = $pdo->prepare($query);
-        $statement->execute(array($user_id, $email, $first_name, $last_name, "default", "", "", "", $encryption_key_, $encryption_iv_, $date, $time, $time_zone, "system"));
+        $statement->execute(array($user_id, $email, $first_name, $last_name, "default", "", "", "", $encryption_key_, $encryption_iv_, "system"));
 
         $query = "delete from login_info where user_id = ?";
         $statement = $pdo->prepare($query);
         $statement->execute(array($user_id));
 
-        $query = "insert into login_info (user_id, device_token, device_brand, device_model, app_version, date, time, time_zone, encryption_key, encryption_iv) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $query = "insert into login_info (user_id, device_token, device_brand, device_model, app_version, encryption_key, encryption_iv) values (?, ?, ?, ?, ?, ?, ?)";
         $statement = $pdo->prepare($query);
-        $statement->execute(array($user_id, $device_token, $device_brand, $device_model, $app_version, $date, $time, $time_zone, $encryption_key_, $encryption_iv_));
+        $statement->execute(array($user_id, $device_token, $device_brand, $device_model, $app_version, $encryption_key_, $encryption_iv_));
 
         $status["response"] = "Done";
     }
