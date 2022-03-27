70eb53f928c9947937441e154c573ddb998471ce
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-27 18:31:25 +0100

    test

--- api/v1/join.php
+++ api/v1/join.php
@@ -3,9 +3,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/api/config/database.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/api/models/data_security.php";
-require_once $_SERVER["DOCUMENT_ROOT"] . "/api/models/users.php";
+require_once "config/database.php";
+require_once "models/data_security.php";
+require_once "models/users.php";
 
 $status = array();
 $database = new database();
