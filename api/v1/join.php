4dc137c8333861d5e47739595c19d1de28e6df63
Chimezie Ugonna <ugiezie@gmail.com>
2022-03-28 12:17:20 +0100

    fix

--- api/v1/join.php
+++ api/v1/join.php
@@ -14,8 +14,7 @@ if ($connection != null) {
     $users = new users($connection);
     $data_security = new data_security();
 
-    if (isset($_SERVER["HTTP_AUTHORIZATION"])) {
-        $status["auth"] = $_SERVER["HTTP_AUTHORIZATION"];
+    if (isset($_SERVER["HTTP_AUTHORIZATION"]) && 0 === stripos($_SERVER["HTTP_AUTHORIZATION"], "bearer ")) {
         if ($_SERVER["REQUEST_METHOD"] == "GET" || $_SERVER["REQUEST_METHOD"] == "POST") {
             if (isset($_POST['user_id']) && $_POST['user_id'] != "" && isset($_POST['full_name']) && $_POST['full_name'] != "" && isset($_POST['email']) && $_POST['email'] != "" && isset($_POST['device_token']) && $_POST['device_token'] != "" && isset($_POST['device_brand']) && $_POST['device_brand'] != "" && isset($_POST['device_model']) && $_POST['device_model'] != "" && isset($_POST['app_version']) && $_POST['app_version'] != "" && isset($_POST['os_version']) && $_POST['os_version'] != "") {
                 $user_id = addslashes($_POST["user_id"]);
