88cd6eee8d7da065e2d0b90a8caecea03a664afa
Chimezie Ugonna <ugiezie@gmail.com>
2022-04-08 20:31:27 +0100

    fix.

--- api/models/login_info.php
+++ api/models/login_info.php
@@ -13,7 +13,8 @@ class login_info
 
     public function insert($user_id, $device_token, $device_brand, $device_model, $app_version, $decryption_key, $decryption_iv, $os_version)
     {
-        if ($this->users->read($user_id)->rowCount() > 0) {
+        $statement = $this->users->read($user_id);
+        if ($statement && $statement->rowCount() > 0) {
             if ($this->delete($user_id)) {
                 $statement = $this->connection->prepare("insert into " . $this->db_table . " (user_id, device_token, device_brand, device_model, app_version, decryption_key, decryption_iv, os_version, done_at) values (?, ?, ?, ?, ?, ?, ?, ?, now())");
                 if ($statement->execute(array($user_id, $device_token, $device_brand, $device_model, $app_version, $decryption_key, $decryption_iv, $os_version))) {
