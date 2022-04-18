7d4198e5a225ea5ce62605a5f5c5c68175966fcf
Chimezie Ugonna <ugiezie@gmail.com>
2022-04-18 10:13:50 +0100

    adjusted the response class.

--- api/models/response.php
+++ api/models/response.php
@@ -4,6 +4,6 @@ class response
     function send($code, $message, $data = array())
     {
         http_response_code($code);
-        return json_encode(array("status" => $code, "message" => $message, "data" => $data));
+        echo json_encode(array("status" => $code, "message" => $message, "data" => $data));
     }
 }
