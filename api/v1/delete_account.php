b6847129a252890e8a0e60515e11a191133b6994
Chimezie Ugonna <ugiezie@gmail.com>
2022-05-05 15:02:51 +0100

    minor fixes.

--- api/v1/delete_account.php
+++ api/v1/delete_account.php
@@ -19,34 +19,40 @@ if ($connection != null) {
 
     if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
         if (isset($_SERVER["HTTP_AUTHORIZATION"])) {
-            list($type, $token) = explode(" ", $_SERVER["HTTP_AUTHORIZATION"], 2);
-            if (strcasecmp($type, "Bearer") == 0) {
-                $data = $authentication->decode($token);
-                if ($data != false) {
-                    $user_id = $data["user_id"];
-                    $statement = $users->read($user_id);
-                    if ($statement != null) {
-                        if ($statement->rowCount() > 0) {
-                            if ($login_info->delete($user_id)) {
-                                if ($users->delete($user_id)) {
-                                    $response->send(200, "Account deleted successfully.");
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+            try {
+                list($type, $token) = explode(" ", $_SERVER["HTTP_AUTHORIZATION"], 2);
+                if (strcasecmp($type, "Bearer") == 0) {
+                    $data = $authentication->decode($token);
+                    if ($data != false) {
+                        $user_id = $data["user_id"];
+                        $statement = $users->read($user_id);
+                        if ($statement != null) {
+                            if ($statement->rowCount() > 0) {
+                                if ($login_info->delete($user_id)) {
+                                    if ($users->delete($user_id)) {
+                                        $response->send(200, "Account deleted successfully.");
+                                    } else {
+                                        $response->send(500, "Account deletion failed.");
+                                    }
                                 } else {
                                     $response->send(500, "Account deletion failed.");
                                 }
                             } else {
-                                $response->send(500, "Account deletion failed.");
+                                $response->send(401, "Unauthorized access, user does not exist.");
                             }
                         } else {
-                            $response->send(401, "Unauthorized access, user does not exist.");
+                            $response->send(500, "Authentication failed.");
                         }
                     } else {
-                        $response->send(500, "Authentication failed.");
+                        $response->send(401, "Bearer token is invalid.");
                     }
                 } else {
-                    $response->send(401, "Bearer token is invalid.");
+                    $response->send(401, "Bearer token is required.");
                 }
-            } else {
-                $response->send(401, "Bearer token is required.");
+            } catch (Exception $e) {
+                echo $e->getMessage();
             }
         } else {
             $response->send(401, "Unauthorized access, token is required.");
