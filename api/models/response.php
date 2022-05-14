a40a18483912b240dd17fdf65548186f44d6a853
Chimezie Ugonna <44467109+chimezie-ugonna@users.noreply.github.com>
2022-05-14 08:03:39 +0100

    Delete api directory

--- api/models/response.php
+++
@@ -1,9 +0,0 @@
-<?php
-class response
-{
-    function send($code, $message, $data = array())
-    {
-        http_response_code($code);
-        echo json_encode(array("status" => $code, "message" => $message, "data" => $data));
-    }
-}
